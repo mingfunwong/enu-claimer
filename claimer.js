3990980cf84a42292d4fd534fb4d02633eed3a8a
qiushaoxi <qiushaoxi@163.com>
2019-04-01 20:22:57 +0800

    do not check

--- claimer.js
+++ claimer.js
@@ -22,87 +22,26 @@ cacheRewards();
 setInterval(cacheRewards, 10 * 60 * 1000 + 5000);
 //////////////////////////
 function cacheRewards() {
-    Promise.all([getGlobal(), getProducer(producerName)]).then(([global, producer]) => {
-        let bpay = (global.perblock_bucket * producer.unpaid_blocks) / global.total_unpaid_blocks / 10000;
-        let vpay = (global.pervote_bucket * producer.total_votes) / (1 * global.total_producer_vote_weight) / 10000;
-        if (vpay < vpayThreshold) {
-            vpay = 0;
-        }
-        let last_time = producer.last_claim_time / 1000;
-        let next_claim_time = 1 * last_time + 24 * 60 * 60 * 1000;
-        console.log("now:", Date.now());
-        console.log("next_claim_time:", next_claim_time);
-        if (next_claim_time > Date.now()) {
-            return 0;
-        }
-        return bpay + vpay;
-    }, errs => {
-        console.error(errs);
-        //retry
-        cacheRewards();
-    }).then(rewards => {
-        console.log("current rewards:", rewards);
-        if (rewards > 0) {
-            eos.transaction({
-                // ...headers,
-                actions: [
-                    {
-                        account: systemContract,
-                        name: 'claimrewards',
-                        authorization: [{
-                            actor: producerName,
-                            permission: permission
-                        }],
-                        data: {
-                            owner: producerName
-                        }
-                    }
-                ]
-            }).then(res => {
-                console.log(res);
-            }, err => {
-                console.error(err);
-                //retry
-                // cacheRewards();
-            });
-        }
-    });
-}
-
-function getGlobal() {
-    return new Promise((resolve, reject) => {
-        eos.getTableRows({
-            "scope": systemContract,
-            "code": systemContract,
-            "table": "global",
-            "json": true
-        }).then(res => {
-            resolve(res.rows[0]);
-        }, err => {
-            console.error(err);
-            reject(err);
-        });
-    });
-}
-
-function getProducer(name) {
-    return new Promise((resolve, reject) => {
-        eos.getTableRows({
-            "scope": systemContract,
-            "code": systemContract,
-            "table": "producers",
-            "lower_bound": name,
-            "limit": 1,
-            "json": true
-        }).then(res => {
-            if (!res.rows[0] || name != res.rows[0].owner) {
-                reject("producer not exist!");
+    eos.transaction({
+        // ...headers,
+        actions: [
+            {
+                account: systemContract,
+                name: 'claimrewards',
+                authorization: [{
+                    actor: producerName,
+                    permission: permission
+                }],
+                data: {
+                    owner: producerName
+                }
             }
-            resolve(res.rows[0]);
-        }, err => {
-            console.error(err);
-            reject(err);
-        });
+        ]
+    }).then(res => {
+        console.log(res);
+    }, err => {
+        console.error(err);
+        //retry
+        // cacheRewards();
     });
 }
-
