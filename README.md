70b74599d6864f6210fb0657cc72f0cc9a294535
Mingfun Wong <mingfun.wong.chn@gmail.com>
2021-08-06 23:17:16 +0900

    add docker

--- README.md
+++ README.md
@@ -1,13 +1,3 @@
-# EOS claimer
+# enu-auto-claimbonus
 
-Node script for EOS BP to claim their rewards automatically.
-
-change your owner info in config.json.
-
-## add claimer auth
-
-```
-cleos -u HTTP_ENDPOINT set account permission YOUR_PRODUCER_NAME claimer '{"threshold":1,"keys":[{"key":"YOUR_CLAIMER_PUBLIC_KEY","weight":1}]}' "active" -p YOUR_PRODUCER_NAME@active 
-
-cleos -u HTTP_ENDPOINT set action permission YOUR_PRODUCER_NAME SYSTEM_CONTRACT claimrewards claimer
-```
+copy `.env.sample` to `.env` and set it.
--- claimer.js
+++ claimer.js
@@ -1,46 +1,47 @@
+require('dotenv').config();
 const Eos = require('eosjs');
-const config = require('./config.json');
 
 cacheRewards();
 //try every 10 min
 setInterval(cacheRewards, 10 * 60 * 1000);
 //////////////////////////
 async function cacheRewards() {
+  const { httpEndPoint, chainId, wif, producerName, permission, keyPrefix, systemContract } = process.env;
 
-    for (const claimer of config.claimers) {
+  const eos = Eos({
+    httpEndpoint: httpEndPoint,
+    chainId: chainId,
+    keyProvider: wif,
+    keyPrefix: keyPrefix,
+  });
 
-        const { httpEndPoint, chainId, wif, producerName, permission, keyPrefix, systemContract } = claimer
-
-
-        const eos = Eos({
-            httpEndpoint: httpEndPoint,
-            chainId: chainId,
-            keyProvider: wif,
-            keyPrefix: keyPrefix
-        });
-
-        await eos.transaction({
-            // ...headers,
-            actions: [
-                {
-                    account: systemContract,
-                    name: 'claimrewards',
-                    authorization: [{
-                        actor: producerName,
-                        permission: permission
-                    }],
-                    data: {
-                        owner: producerName
-                    }
-                }
-            ]
-        }).then(res => {
-            console.log(res);
-        }, err => {
-            console.error(err);
-            //retry
-            // cacheRewards();
-        });
-
-    }
+  await eos
+    .transaction({
+      // ...headers,
+      actions: [
+        {
+          account: systemContract,
+          name: 'claimrewards',
+          authorization: [
+            {
+              actor: producerName,
+              permission: permission,
+            },
+          ],
+          data: {
+            owner: producerName,
+          },
+        },
+      ],
+    })
+    .then(
+      res => {
+        console.log(res);
+      },
+      err => {
+        console.error(err);
+        //retry
+        // cacheRewards();
+      }
+    );
 }
